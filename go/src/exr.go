d855b184abc43a8faf65bffcee4feb7ace1118f2
curoles <curoles@yahoo.com>
2017-07-16 15:53:25 -0700

    thinking about json

--- go/src/exr.go
+++ go/src/exr.go
@@ -7,6 +7,7 @@ package main
 import (
     "os"
     "path"
+    "fmt"
     "html/template"
     "net/http"
 )
@@ -16,25 +17,50 @@ type Page struct {
     Content string
 }
 
+func getPathToTemplate() string {
+
+    pathToBinary := path.Dir(os.Args[0])
+    pathToTemplates := path.Join(pathToBinary,"html_templates")
+    pathToTemplate := path.Join(pathToTemplates, "exr.html")
+
+    return pathToTemplate
+}
+
+func getPathToExercises() string {
+    pathToBinary := path.Dir(os.Args[0])
+    pathToExercises := path.Join(pathToBinary,"exercise")
+
+    return pathToExercises
+}
+
+// Store ready-to-use template in variable
+var templateInstance = template.Must(template.ParseFiles(getPathToTemplate()))
+
+// Runs before main and after all packages vars initialized.
+func init() {
+    fmt.Println("Initializing...")
+}
+
 func displayPage(
     writer   http.ResponseWriter,
     request *http.Request) {
+
+    fmt.Println("Request: ", request.URL)
 
     page := &Page {
         Title: "exercise",
         Content: "content todo",
     }
 
-    pathToBinary := path.Dir(os.Args[0])
-    pathToTemplates := path.Join(pathToBinary,"html_templates")
-    pathToTemplate := path.Join(pathToTemplates, "exr.html")
-
-    template := template.Must(template.ParseFiles(pathToTemplate))
-
-    template.Execute(writer, page)
+    templateInstance.Execute(writer, page)
 }
 
 func main() {
+    exercisesJsonDir := http.Dir(getPathToExercises())
+    exrJsonHandler := http.StripPrefix("/exercise/", http.FileServer(exercisesJsonDir))
+    http.Handle("/exercise/", exrJsonHandler)
+
     http.HandleFunc("/", displayPage)
+    fmt.Println("Serving HTTP requests")
     http.ListenAndServe(":8080", nil)
 }
